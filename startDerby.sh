3aa700fc5c9bf2d5d04e6040d4f30bc4ce6c7df3
abaxsoraszem <abax.soraszem@gmail.com>
2014-12-14 16:40:20 +0100

    database update for latest changes

--- startDerby.sh
+++ startDerby.sh
@@ -1,11 +1,11 @@
-export DEV_ANON=/Users/csaba/dev_anon
-
+export DEV_ANON=$HOME/dev_anon
 export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_55.jdk/Contents/Home
+
 export M2_HOME=$DEV_ANON/maven
 export PATH=$JAVA_HOME/bin:$M2_HOME/bin:$PATH
 export DERBY_HOME=$DEV_ANON/db-derby-10.4.2.0-bin
 export CLASSPATH=$DERBY_HOME/lib/derbytools.jar:$DERBY_HOME/lib/derbynet.jar:.
 
-cd $DEV_ANON/projects
+cd $DEV_ANON/anon-repo-db
 
 $DERBY_HOME/bin/startNetworkServer
